b71bda7c085806909f14e0f93bc6cb0fe05bcf93
AbsurdCactus <braydonreber@gmail.com>
2021-11-10 12:16:09 -0500

    Fixed AssertTrue error, needs more tests to finish

--- test.py
+++ test.py
@@ -1,15 +1,37 @@
 import unittest
 import main
 
+
 class TestMain(unittest.TestCase):
 
     def test_pale_ple(self):
 
-        self.assertTrue(main.palindrome_detector(string_one="pale", string_two="ple"), True)
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="ple"), True)
 
-     # this should not be working yet   
-    def test_pale_bake(self):
+    def test_pale_palest(self):
 
-         self.assertTrue(main.palindrome_detector(string_one="pale", string_two="bake"), False)
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="palest"), False)
 
-        
+    def test_pale_pa(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="pa"), False)
+
+    def test_pa_pale(self):
+        self.assertEqual(main.palindrome_detector(
+            string_one="pa", string_two="pale"), False)
+
+    def test_pale_bale(self):
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="bale"), True)
+
+    def test_pale_bane(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="bane"), False)
+
+    #there need to be a lot more tests
+
+
