232b56442a3e770b7a80d4c8cb855b2033eb009a
Absurdcactus <braydonreber@gmail.com>
2022-03-02 17:04:53 -0500

    Remove typo code from One_away challenge

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Palindrome-Permutation
+
+Given a string, write a function to check if is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. You can ignore casing and non-letter characters.
+
+Taken from page 91 of *Cracking the Coding Interview*.
--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 
 def palindrome_detector(string_one, string_two):
-
-    string_list = [string_one, string_two]
-    longest = max(string_list)
-    shortest = min(string_list)
-
-    if len(longest) - len(shortest) > 1:
-
-        return False
-
-    char_change_counter = 0
-
-    for i in longest:
-        
-        if i not in shortest:
-
-            char_change_counter += 1
-
-    if char_change_counter > 1:
-
-        print(char_change_counter)
-
-        return False
-
-    else:
-
-        return True
+    
+    return False
 
 
 
